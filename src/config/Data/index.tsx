62e0a9e85118c5934ceef1c68d9a0cafc4c5d28a
atsss <atsushi0623itoh@gmail.com>
2020-09-12 18:01:15 -0600

    :+1: コミュニティーページ用のサンプルデータ作成

---
+++ src/config/Data/index.tsx
@@ -0,0 +1,28 @@
+export const user = {
+  id: 1,
+  name: '美甘良夫',
+  image: 'https://cocchi-sample.s3-us-west-2.amazonaws.com/user.jpg',
+}
+export const communities = [
+  {
+    id: 1,
+    name: 'マイマップ',
+    image: 'https://cocchi-sample.s3-us-west-2.amazonaws.com/communitiy1.jpg',
+    members: 'Only you',
+    num_of_folders: 2,
+  },
+  {
+    id: 2,
+    name: '大学いつメン',
+    image: 'https://cocchi-sample.s3-us-west-2.amazonaws.com/communitiy2.jpg',
+    members: 'You, 佐藤舞 and 伊藤優',
+    num_of_folders: 3,
+  },
+  {
+    id: 3,
+    name: 'バイト',
+    image: 'https://cocchi-sample.s3-us-west-2.amazonaws.com/communitiy3.jpg',
+    members: 'You, 田中佳子, 山田智子 and 岡田萌',
+    num_of_folders: 2,
+  },
+]
