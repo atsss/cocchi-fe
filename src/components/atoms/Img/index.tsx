b389aff5de797bf0536598e3277cd24247d786ae
atsss <atsushi0623itoh@gmail.com>
2020-09-12 19:27:41 -0600

    :+1: Img コンポーネント作成

---
+++ src/components/atoms/Img/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './styles.module.sass'
+
+interface Props {
+  src: string
+  size?: string
+  alt?: string
+  border?: string
+  className?: string
+}
+
+export const Img: React.FC<Props> = ({
+  src,
+  size = 'm',
+  alt = '',
+  border = 'none',
+  className,
+}) => (
+  <img
+    src={src}
+    alt={alt}
+    className={[styles[size], styles[border], className].join(' ')}
+  />
+)
