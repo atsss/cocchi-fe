feb063b19dd9f30f509d505a2ef7d90cf5d2516f
atsss <atsushi0623itoh@gmail.com>
2020-09-13 12:00:31 -0600

    :lipstick: 背景色の修正

--- src/components/templates/communities/Index/index.tsx
+++ src/components/templates/communities/Index/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { User, Community } from '../../../../interfaces'
-import { CommunityHeader } from '../../../organisms/headers/CommunityHeader'
-import { Nav } from '../../../organisms/Nav'
 import { Section } from '../../../atoms/Section'
 import { CommunityList } from '../../../molecules/lists/CommunityList'
+import { CommunityHeader } from '../../../organisms/headers/CommunityHeader'
+import { Nav } from '../../../organisms/Nav'
 
 interface Props {
   user: User
@@ -17,7 +17,7 @@ export const CommunitiesIndexTemplate: React.FC<Props> = ({
   <>
     <CommunityHeader user={user} />
     <Section>
-      {communities.map((community: Community) => (
+      {communities.map((community: Community, index: number) => (
         <CommunityList
           key={community.id}
           id={community.id}
@@ -25,7 +25,7 @@ export const CommunitiesIndexTemplate: React.FC<Props> = ({
           image={community.image}
           members={community.members}
           numOfFolders={community.numOfFolders}
-          className="u-mt32"
+          className={index === 0 ? '' : 'u-mt24'}
         />
       ))}
     </Section>
