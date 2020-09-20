6fc1a82aeaac8f924911febcdefbb143f3ff6e8a
atsss <atsushi0623itoh@gmail.com>
2020-09-20 16:16:12 -0600

    :+1: ChannelHeader コンポーネント作成

---
+++ src/components/organisms/headers/ChannelHeader/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+/* import { BsThreeDots } from 'react-icons/bs' */
+import styles from './styles.module.sass'
+import { Community } from '../../../../interfaces'
+import { Header } from '../../../atoms/Header'
+import { Img } from '../../../atoms/Img'
+/* import { Link } from '../../../atoms/Link' */
+import { Txt } from '../../../atoms/Txt'
+
+interface Props {
+  community: Community
+}
+
+export const ChannelHeader: React.FC<Props> = ({ community }) => (
+  <Header>
+    <div className={styles.block}>
+      <div className={styles.block}>
+        <Img src={community.image} size="s" border="radius" className="u-mr8" />
+        <Txt weight="bold">{community.name}</Txt>
+      </div>
+      {/* <div className={styles.icon}> */}
+      {/*   <Link to={`/communitys/${community.id}/edit`}> */}
+      {/*     <BsThreeDots /> */}
+      {/*   </Link> */}
+      {/* </div> */}
+    </div>
+  </Header>
+)
