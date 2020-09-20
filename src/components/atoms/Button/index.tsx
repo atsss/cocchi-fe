a7b298bb7720b05c1ff73af6cea6db00eca8e701
atsss <atsushi0623itoh@gmail.com>
2020-09-20 17:17:11 -0600

    :lipstick: BlockButton コンポーネント作成

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button as DefaultButton } from '@adobe/react-spectrum'
+import styles from './styles.module.sass'
 
 interface Props {
   onPress: () => void
@@ -9,6 +10,18 @@ interface Props {
 
 export const Button: React.FC<Props> = ({ onPress, children, className }) => (
   <div className={className}>
+    <DefaultButton onPress={onPress} variant="primary" isQuiet>
+      {children}
+    </DefaultButton>
+  </div>
+)
+
+export const BlockButton: React.FC<Props> = ({
+  onPress,
+  children,
+  className,
+}) => (
+  <div className={[styles.block, className].join(' ')}>
     <DefaultButton onPress={onPress} variant="primary" isQuiet>
       {children}
     </DefaultButton>
--- src/components/templates/communities/Index/index.tsx
+++ src/components/templates/communities/Index/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { User, Community } from '../../../../interfaces'
 import { Section } from '../../../atoms/Section'
+import { BlockButton } from '../../../atoms/Button'
 import { CommunityList } from '../../../molecules/lists/CommunityList'
 import { CommunityHeader } from '../../../organisms/headers/CommunityHeader'
 import { Nav } from '../../../organisms/Nav'
@@ -13,22 +15,30 @@ interface Props {
 export const CommunitiesIndexTemplate: React.FC<Props> = ({
   user,
   communities,
-}) => (
-  <>
-    <CommunityHeader user={user} />
-    <Section>
-      {communities.map((community: Community, index: number) => (
-        <CommunityList
-          key={community.id}
-          id={community.id}
-          name={community.name}
-          image={community.image}
-          members={community.members}
-          numOfFolders={community.numOfFolders}
-          className={index === 0 ? '' : 'u-mt24'}
-        />
-      ))}
-    </Section>
-    <Nav />
-  </>
-)
+}) => {
+  const history = useHistory()
+
+  return (
+    <>
+      <CommunityHeader user={user} />
+      <Section>
+        {communities.map((community: Community, index: number) => (
+          <BlockButton
+            onPress={() => history.push(`communities/${community.id}/channels`)}
+            className={index === 0 ? '' : 'u-mt16'}
+          >
+            <CommunityList
+              key={community.id}
+              id={community.id}
+              name={community.name}
+              image={community.image}
+              members={community.members}
+              numOfFolders={community.numOfFolders}
+            />
+          </BlockButton>
+        ))}
+      </Section>
+      <Nav />
+    </>
+  )
+}
