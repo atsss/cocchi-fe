8b6c6c33af1c6a6c29ff980b3ed79328e9c41307
Yoshi19950901 <volc0m.ym@gmail.com>
2020-09-26 16:12:07 -0600

    フォルダー数の表示

--- src/components/organisms/headers/ChannelHeader/index.tsx
+++ src/components/organisms/headers/ChannelHeader/index.tsx
@@ -17,6 +17,7 @@ export const ChannelHeader: React.FC<Props> = ({ community }) => (
       <div className={styles.block}>
         <Img src={community.image} size="s" className="u-mr8" />
         <Txt weight="bold">{community.name}</Txt>
+        <Txt size="s">{`${community.numOfFolders} folders`}</Txt>
       </div>
       {/* <div className={styles.icon}> */}
       {/*   <Link to={`/communitys/${community.id}/edit`}> */}
