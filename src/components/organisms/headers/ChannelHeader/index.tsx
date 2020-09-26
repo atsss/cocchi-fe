29d2d51768dedecb7e56448215afaff35cbfae9e
Yoshi19950901 <volc0m.ym@gmail.com>
2020-09-26 16:29:00 -0600

    縦並びに変更

--- src/components/organisms/headers/ChannelHeader/index.tsx
+++ src/components/organisms/headers/ChannelHeader/index.tsx
@@ -16,8 +16,10 @@ export const ChannelHeader: React.FC<Props> = ({ community }) => (
     <div className={styles.block}>
       <div className={styles.block}>
         <Img src={community.image} size="s" className="u-mr8" />
-        <Txt weight="bold">{community.name}</Txt>
-        <Txt size="s">{`${community.numOfFolders} folders`}</Txt>
+        <div>
+          <Txt weight="bold">{community.name}</Txt>
+          <Txt size="s">{`${community.numOfFolders} folders`}</Txt>
+        </div>
       </div>
       {/* <div className={styles.icon}> */}
       {/*   <Link to={`/communitys/${community.id}/edit`}> */}
