247cbcd9cc95583a9ed1ab5cb62bc702145c19ba
Yoshi19950901 <volc0m.ym@gmail.com>
2020-10-20 21:36:47 -0600

    Place#showコメントアウト削除

--- src/components/pages/places/Show/index.tsx
+++ src/components/pages/places/Show/index.tsx
@@ -6,7 +6,6 @@ import { places } from '../../../../config/data'
 export const PlacesShow: React.FC = () => {
   const { id } = useParams()
   const place = places.find((element) => element.id === parseInt(id, 10))
-  // const tmpChannel = { id: 0, name: 'すべて', icon: 'all' , numOfPlaces: 0 }
 
   if (!place) return <p>error</p>
   return <Template place={place} />
