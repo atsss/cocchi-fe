0a47f9d53594b085bfee3f7b3f1b4de4eca79a8e
Yoshi19950901 <volc0m.ym@gmail.com>
2020-11-05 23:10:54 -0600

    引数削除

--- src/components/pages/places/New/index.tsx
+++ src/components/pages/places/New/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
 import { PlacesNewTemplate as Template } from '../../../templates/places/New'
-import { places } from '../../../../config/data'
 
-export const PlacesNew: React.FC = () => <Template place={places} />
+export const PlacesNew: React.FC = () => <Template />
--- src/components/templates/places/New/index.tsx
+++ src/components/templates/places/New/index.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
-import { Place } from '../../../../interfaces'
 import { EditHeader } from '../../../organisms/headers/EditHeader'
 import { Nav } from '../../../organisms/Nav'
 
-interface Props {
-  place: Place[]
-}
-
-export const PlacesNewTemplate: React.FC<Props> = () => (
+export const PlacesNewTemplate: React.FC = () => (
   <>
     <EditHeader backPath="/">新規ピン</EditHeader>
     <Nav />
