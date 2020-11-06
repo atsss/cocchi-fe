472f8802c40da031bdc886a6daa2ef4c29fcd9e4
Yoshi19950901 <volc0m.ym@gmail.com>
2020-11-05 21:59:02 -0600

    仮想的に設定した不要なもの&引数を削除

--- src/components/pages/places/New/index.tsx
+++ src/components/pages/places/New/index.tsx
@@ -1,14 +1,5 @@
 import React from 'react'
 import { PlacesNewTemplate as Template } from '../../../templates/places/New'
-// import { places } from '../../../../config/data'
+import { places } from '../../../../config/data'
 
-export const PlacesNew: React.FC = () => {
-  const places = {
-    id: 0,
-    name: 'starbacks',
-    image: 'https://cocchi-sample.s3-us-west-2.amazonaws.com/community3.jpg',
-    visit: '行った',
-  }
-
-  return <Template place={places} />
-}
+export const PlacesNew: React.FC = () => <Template place={places} />
